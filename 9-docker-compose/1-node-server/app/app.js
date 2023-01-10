ac40f9677a1c062a60e1a2c45bf3fe89d88c6650
Burak Çakır <burak2043@gmail.com>
2023-01-10 15:06:01 +0300

    docker-compose started

---
+++ 9-docker-compose/1-node-server/app/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("hello world");
+})
+app.listen(PORT, () => {
+    console.log(`Server http://localhost:${PORT} da calisti...`);
+})
